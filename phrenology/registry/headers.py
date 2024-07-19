40d1b33d661923398d5c10137184415d466a540d
Austin Leahy <leahy@apache.org>
2024-07-19 14:33:52 -0500

    established registry

---
+++ phrenology/registry/headers.py
@@ -0,0 +1,45 @@
+EXPECTED_HEADERS = {
+                        "result":["success","error"],
+                        "items":[
+                            "X-Frame-Options", 
+                            "X-Content-Type-Options", 
+                            "Strict-Transport-Security",
+                            "Permissions-Policy", 
+                            "X-Frame-Options", 
+                            "Strict-Transport-Security",
+                            "Content-Security-Policy", 
+                            "Cross-Origin-Embedder-Policy", 
+                            "Cross-Origin-Resource-Policy",
+                            "Cross-Origin-Opener-Policy",
+                            "Referrer-Policy"
+                            ]
+                    }
+
+DEPRICATED_HEADERS = {
+                        "result":["warn","success"],
+                        "items":[
+                            "X-XSS-Protection", 
+                            "Expect-CT", 
+                            "X-Permitted-Cross-Domain-Policies"
+                            ]
+                    }
+
+INFORMATION_HEADERS = {
+                            "result":["info","info"],
+                            "items":[
+                                "X-Powered-By", 
+                                "Server", 
+                                "x-AspNet-Version", 
+                                "X-AspNetMvc-Version"
+                                ]
+                        }
+CACHE_HEADERS = {
+                    "result":["info","info"],
+                    "items":[
+                        "Cache-Control", 
+                        "Pragma", 
+                        "Last-Modified", 
+                        "Expires", 
+                        "ETag"]
+                }
+          
