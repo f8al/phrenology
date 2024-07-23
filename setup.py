ccfcdff48e9763740e7cc905159270ccde13d6ea
f8al <securityshrimp@proton.me>
2024-07-23 11:50:46 -0500

    correct variable case and add docstring

--- setup.py
+++ setup.py
@@ -1,12 +1,32 @@
 from setuptools import setup, find_packages
+'''
+**phrenology - A tool for analyzing your security HEADers**
+
+**phrenology** is a Python package that helps you analyze the security headers of a website. It provides functionalities to fetch headers, 
+parse them, and identify potential security vulnerabilities.
+
+**Key Features:**
+
+* Fetches website headers using requests library.
+* Parses retrieved headers and extracts relevant information.
+* Analyzes headers for potential security weaknesses based on best practices.
+
+**Installation:**
+
+You can install phrenology using pip:
+
+```bash
+pip install phrenology
+```
+'''
 
 # Replace with your project information
-project_name = "phrenology"
-version = "0.1.1"
-description = "A tool for analyzing your security HEADers"
-author = "SecurityShrimp"
-author_email = "securityshrimp@proton.me"
-url = "https://github.com/f8al/phrenology"  # Optional
+PROJECT_NAME = "phrenology"
+VERSION = "0.1.1"
+DESCRIPTION = "A tool for analyzing your security HEADers"
+AUTHOR = "SecurityShrimp"
+AUTHOR_EMAIL = "securityshrimp@proton.me"
+URL = "https://github.com/f8al/phrenology"  # Optional
 # license = "MIT"  # Optional
 
 classifiers = [
@@ -31,12 +51,12 @@ dependencies = [
 ]
 
 setup(
-    name=project_name,
-    version=version,
-    description=description,
-    author=author,
-    author_email=author_email,
-    url=url,
+    name=PROJECT_NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
     #license=license,
     packages=find_packages(exclude=("tests*",)),  # Exclude test directories
     install_requires=dependencies,
